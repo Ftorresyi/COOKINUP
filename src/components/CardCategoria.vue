c2c2bba77a768cfe9612f0d7a26d33df7a8ef3f1
yindi <yindiyft@gmail.com>
2024-06-27 18:31:39 -0300

    Ajuste Color ingrediente e imagens dinamicas

--- src/components/CardCategoria.vue
+++ src/components/CardCategoria.vue
@@ -15,7 +15,7 @@ export default {
     <!--  {{ categoria.nome }} -->
     <article class="categoria">
         <header class="categoria__cabecalho">
-            <img :src="`/images/icones/categorias_ingredientes/${categoria.imagem}`" alt="" class="categoria__imagem">
+            <img :src="`/imagens/icones/categorias_ingredientes/${categoria.imagem}`" alt="" class="categoria__imagem">
 
             <h2 class="paragrafo-lg categoria__nome"> {{ categoria.nome }} </h2>
         </header>
@@ -26,3 +26,44 @@ export default {
         </ul>
     </article>
 </template>
+
+<style scoped>
+.categoria {
+  width: 19.5rem;
+  padding: 1rem;
+  border-radius: 1rem;
+  background: var(--branco, #FFF);
+  box-shadow: 4px 4px 10px 0px rgba(68, 68, 68, 0.05);
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+}
+
+.categoria__cabecalho {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+}
+
+.categoria__imagem {
+  width: 3.5rem;
+}
+
+.categoria__nome {
+  text-align: center;
+  color: var(--verde-medio, #17e04d);
+  font-weight: 700;
+}
+
+.categoria__ingredientes {
+  display: flex;
+  justify-content: center;
+  gap: 0.5rem;
+  flex-wrap: wrap;
+  color:#17e04d;
+}
+</style>
